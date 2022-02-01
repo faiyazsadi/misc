19384938d1a88c409c3a8c0bcb79cb35b32658c6
Sadi <faiyazsadi3334@gmail.com>
2022-02-01 11:41:19 +0600

    filename extractor max pro

--- filename_extractor.cpp
+++ filename_extractor.cpp
@@ -1,11 +1,14 @@
-#include <iostream>
 #include <string>
+#include <iostream>
 #include <filesystem>
+
+namespace fs = std::filesystem;
 
 int main() {
     std::string path = "D:\\Academics\\2-1\\Previous Year Questions";
-    for(const auto & entry : std::filesystem::directory_iterator(path)) {
-        std::filesystem::path p = entry.path();
+    for(const auto & entry : fs::recursive_directory_iterator(path)) {
+        fs::path p = entry.path();
+        if(fs::is_directory(p)) continue;
         std::cout << p.filename().string() << std::endl;
     }
     return 0;
